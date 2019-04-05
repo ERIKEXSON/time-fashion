22ad93f5eacc6d22feee52e5993767c2561bf517
Aprendiz <Aprendiz@SGLDFPCAT7F593.FormacionSena.RED>
2019-04-05 17:54:15 -0500

    admin terminado a espera de revision 2

--- time-fashion-frontend/src/views/admin/Empresas.vue
+++ time-fashion-frontend/src/views/admin/Empresas.vue
@@ -25,34 +25,55 @@
           <td class="text-xs-left">{{ props.item.nit }}</td>
           <td class="text-xs-left">{{ props.item.telefono }}</td>
           <td class="text-xs-left">{{ props.item.correo }}</td>
-          <v-btn to="/admin/empresas/empleados" outline>Empleados</v-btn>
-          <!--<a href="javascript:;" @click.stop="terms = true">terms</a>-->
+          <div class="botonEmpleados"><v-btn @click="empleados = true">Empleados</v-btn></div>
         </template>
         </v-data-table>
       </v-card>
+      <v-dialog v-model="empleados" width="1085">
+        <v-card>
+          <v-card-text>
+            <nav style="border: #000000 3px solid">
+              <div style="background-color:#000000;padding: 5px;color: white"><h2>Empleados</h2></div>
+              <v-card width="1045">
+                <v-card-title>
+                  <v-text-field
+                    v-model="search"
+                    append-icon="search"
+                    label="Buscar"
+                    single-line
+                    hide-details
+                  ></v-text-field>
+                </v-card-title>
+                <v-data-table
+                  :headers="encabezados"
+                  :items="contenido"
+                  :search="search"
+                >
+                  <template v-slot:items="props">
+                    <td class="text-xs-left">{{ props.item.name }}</td>
+                    <td class="text-xs-left">{{ props.item.apellido }}</td>
+                    <td class="text-xs-left">{{ props.item.correo }}</td>
+                    <td class="text-xs-left">{{ props.item.direccion }}</td>
+                    <td class="text-xs-left">{{ props.item.telefono }}</td>
+                  </template>
+                </v-data-table>
+              </v-card>
+            </nav>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <div class="botonCerrar"><v-btn flat @click="empleados = false">Cerrar</v-btn></div>
+            </v-card-actions>
+          </v-card-text>
+        </v-card>
+      </v-dialog>
     </nav>
-    <!--<v-dialog v-model="terms" width="70%">
-      <v-card>
-        <v-card-title class="title">Terms</v-card-title>
-        <v-card-text v-for="n in 5" :key="n">
-          {{ content }}
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn
-            flat
-            color="purple"
-            @click="terms = false"
-          >Ok</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>-->
   </v-app>
 </template>
 <script>
 export default {
   data () {
     return {
+      empleados: false,
       search: '',
       headers: [
         { text: 'Nombre', value: 'name' },
@@ -68,6 +89,22 @@ export default {
           telefono: '321564651',
           correo: 'jbfjhbue@erjg.com'
         }
+      ],
+      encabezados: [
+        { text: 'Nombre', value: 'nombre' },
+        { text: 'Apellido', value: 'apellido' },
+        { text: 'Correo', value: 'correo' },
+        { text: 'Dirección', value: 'direccion' },
+        { text: 'Teléfono', value: 'telefono' }
+      ],
+      contenido: [
+        {
+          name: 'Errik',
+          apellido: 'Marto',
+          correo: 'asdasda@hola.com',
+          direccion: 'asdasdasd123123',
+          telefono: '123123123'
+        }
       ]
     }
   },
@@ -76,3 +113,21 @@ export default {
   }
 }
 </script>
+<style>
+  .botonCerrar{
+    background-color: rgba(34, 194, 215, 0.61);
+    transition: all .2s linear
+  }
+  .botonCerrar:hover{
+    background-color: rgb(145, 45, 45)
+  }
+  .botonEmpleados{
+    display: inline-block;
+    margin-right: 40px;
+    background-color: rgba(34, 194, 215, 0.61);
+    transition: all .2s linear
+  }
+  .botonEmpleados:hover{
+    background-color: rgb(145, 45, 45)
+  }
+</style>
