f042cd2e66435ab67470968642db42456741af7e
Aprendiz <Aprendiz@SGLDFPCAT7F577.FormacionSena.RED>
2019-04-06 14:31:06 -0500

    correcion base de datos2

--- backend/db/models/cancellation.model.js
+++ backend/db/models/cancellation.model.js
@@ -5,13 +5,13 @@ const setupDatabase = require('../lib/db')
 
 module.exports = function setupMetricModel (config) {
   const sequelize = setupDatabase(config)
-  return sequelize.define('cancellation', {
+  return sequelize.define('cancellation',{
     uuid: {
       type: Sequelize.CHAR(36),
       defaultValue: Sequelize.UUIDV4
     },
-    fecha: {
-      type: Sequelize.date,
+    fecha:{
+      type: Sequelize.DATE,  
       allowNull: false
     },
   })
