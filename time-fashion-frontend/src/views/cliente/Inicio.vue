6816a4ed2f3b957c034c261fd18c53dde46395b3
Aprendiz <Aprendiz@SGLDFPCAT7F593.FormacionSena.RED>
2019-04-24 17:56:01 -0500

    entrada a cliente

--- time-fashion-frontend/src/views/cliente/Inicio.vue
+++ time-fashion-frontend/src/views/cliente/Inicio.vue
@@ -1,14 +1,28 @@
 <template>
-    <v-app>
-      <v-card>
-
-      </v-card>
-    </v-app>
+  <v-app>
+    <div class="imagenInicio">
+      <img src="@/assets/perfil.png" width="100" height="100">
+      <h2>Bienvenido, {{ usuario }}</h2>
+      <h3>Gestiona tu información para mejorar tu experiencia</h3>
+    </div>
+  </v-app>
 </template>
 <script>
 export default {
+  data () {
+    return {
+      usuario: 'cualquiera'
+    }
+  },
   created () {
     this.$store.commit('SET_LAYOUT', 'cliente-layout')
   }
 }
 </script>
+<style>
+  .imagenInicio{
+    text-align: center;
+    width: fit-content;
+    margin: auto
+  }
+</style>
