7a64e6c22825a9d0d07faa58f022eb6bd200ed80
Aprendiz <Aprendiz@SGLDFPCAT7F577.FormacionSena.RED>
2019-05-03 17:56:36 -0500

    muestreo base de datos 2

--- backend/db/lib/brands.js
+++ backend/db/lib/brands.js
@@ -5,17 +5,12 @@ function setupBrand(brandModel) {
     const result  = await brandModel.create(brand)
     return result.toJSON()
   }
-<<<<<<< HEAD
+
   async function updateBrand(uuid, brand) {
     const cond = { where : { uuid } }
     const result = await BrandModel.update(brand, cond)
     return result ? BrandModel.findOne(cond) :  new Error ('no se actualizo ningun registro')
   }
-
-
-=======
-  
->>>>>>> f6b458d47432933c8cea68bd4a39672e447ec3b4
   return {
     createBrand,
     updateBrand,
