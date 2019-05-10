b52ff5e284a6d6066d7a6bf9b30824c78661fb92
Aprendiz <Aprendiz@SGLDFPCAT7F593.FormacionSena.RED>
2019-05-10 15:03:46 -0500

    cambio contraseña cliente

--- time-fashion-frontend/src/views/pedidos.vue
+++ time-fashion-frontend/src/views/pedidos.vue
@@ -35,14 +35,14 @@
   </v-tabs>
 </template>
 <script>
-  export default {
-    data () {
-      return {
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-      }
-    },
-    created () {
+export default {
+  data () {
+    return {
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    }
+  },
+  created () {
     this.$store.commit('SET_LAYOUT', 'principal-layout')
   }
-  }
+}
 </script>
