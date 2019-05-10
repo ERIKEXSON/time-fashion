17c49bc7fedb4c2e6c8871ed55d90d9dd1bfc1ed
sebastian arias <jsarias98@misena.edu.co>
2019-05-10 16:05:06 -0500

    validar contraseña

--- time-fashion-frontend/src/views/cliente/CambiarPassword.vue
+++ time-fashion-frontend/src/views/cliente/CambiarPassword.vue
@@ -1,87 +1,38 @@
 <template>
   <v-app>
-    <v-form ref="form" @submit.prevent="submit">
-      <v-card width="945">
-        <v-card-actions>
-          <v-container>
-            <v-layout row>
-              <v-flex md1></v-flex>
-              <v-flex md3>
-                <v-subheader>Contraseña actual</v-subheader>
-              </v-flex>
-              <v-flex md1></v-flex>
-              <v-flex md6>
-                <v-text-field v-model="form.currentPassword" required box></v-text-field>
-              </v-flex>
-            </v-layout>
-            <v-layout row>
-              <v-flex md1></v-flex>
-              <v-flex md3>
-                <v-subheader>Contraseña nueva</v-subheader>
-              </v-flex>
-              <v-flex md1></v-flex>
-              <v-flex md6>
-                <v-text-field
-                  :error-messages="passErrors"
-                  v-model="form.newPassword"
-                  :counter="6"
-                  @input="$v.password.$touch()"
-                  @blur="$v.password.$touch()"
-                  required
-                  box
-                ></v-text-field>
-              </v-flex>
-            </v-layout>
-            <v-layout row>
-              <v-flex md1></v-flex>
-              <v-flex md3>
-                <v-subheader>Confirmar contraseña nueva</v-subheader>
-              </v-flex>
-              <v-flex md1></v-flex>
-              <v-flex md6>
-                <v-text-field
-                  :error-messages="matchPass"
-                  v-model="form.newPasswordConfirmed"
-                  @input="$v.repeatPassword.$touch()"
-                  @blur="$v.repeatPassword.$touch()"
-                  required
-                  box
-                ></v-text-field>
-              </v-flex>
-            </v-layout>
-          </v-container>
-        </v-card-actions>
-        <v-card-actions>
-          <div class="boton">
-            <v-btn flat :disabled="!formIsValid" color="black" type="submit">Cambiar contraseña</v-btn>
-          </div>
-        </v-card-actions>
-      </v-card>
-    </v-form>
+    <v-text-field
+      v-model="password"
+      :error-messages="passErrors"
+      :counter="10"
+      label="Nuevo password"
+      required
+      @input="$v.password.$touch()"
+      @blur="$v.password.$touch()"
+    ></v-text-field>
+    <v-text-field
+      v-model="repeatPassword"
+      :error-messages="matchPass"
+      :counter="10"
+      label="Repetir password"
+      required
+      @input="$v.repeatPassword.$touch()"
+      @blur="$v.repeatPassword.$touch()"
+    ></v-text-field>
+    <v-btn
+      :disabled="$v.$invalid"
+      color="success"
+      v-text="'dsad'"
+    />
   </v-app>
 </template>
 <script>
-
 import { required, sameAs, minLength } from 'vuelidate/lib/validators'
 
 export default {
   data () {
-    const defaultForm = Object.freeze({
-      currentPassword: '',
-      newPassword: '',
-      newPasswordConfirmed: ''
-    })
     return {
-      prueba: false,
-      form: Object.assign({}, defaultForm),
-      rules: {
-        required: [
-          val => (val || '').length > 0 || 'Este campo es requerido',
-          val =>
-            (val || '').length > 7 ||
-            'Ingrese contraseña con 8 o mas caracteres'
-        ]
-      }
+      password: '',
+      repeatPassword: ''
     }
   },
   validations: {
@@ -93,22 +44,9 @@ export default {
       sameAsPassword: sameAs('password')
     }
   },
-  methods: {
-    submit () {
-      this.snackbar = true
-      this.resetForm()
-    }
-  },
-  mixins: [validationMixin],
   computed: {
-    formIsValid () {
-      return (
-        this.form.currentPassword &&
-        this.form.newPassword &&
-        this.form.newPasswordConfirmed
-      )
-    },
     passErrors () {
+      console.log(this.$v)
       const errors = []
       if (!this.$v.password.$dirty) return errors
       if (!this.$v.password.minLength) {
@@ -121,11 +59,12 @@ export default {
         errors.push('contraseña requerida')
         return errors
       }
+      return errors
     },
     matchPass () {
       const errors = []
       if (!this.$v.repeatPassword.$dirty) return errors
-      !this.$v.repeatPassword.$dirty && errors.push('diferentes')
+      !this.$v.repeatPassword.sameAsPassword && errors.push('diferentes')
       return errors
     }
   },
