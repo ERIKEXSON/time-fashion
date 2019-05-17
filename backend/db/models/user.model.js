f1dcf06b25efff88eb633e5537c937d8f19b9714
royersgg <rgaravito60@gmail.com>
2019-05-17 12:45:04 -0500

    Areglos base de datos

--- backend/db/models/user.model.js
+++ backend/db/models/user.model.js
@@ -26,6 +26,10 @@ module.exports = function setupMetricModel (config) {
       type: Sequelize.TEXT,
       allowNull: true
     },
+    contraseña: {
+      type: Sequelize.TEXT,
+      allowNull: false
+    },
     rol: {
       type: Sequelize.ENUM('cliente', 'vendedor', 'administrador'),
       allowNull: false,
